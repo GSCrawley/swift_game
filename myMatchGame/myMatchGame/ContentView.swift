0baf28c0eb9bf0967f4bb1a5e532bdc023ed9aa9
GSCrawley <gscrawley@gmail.com>
2021-02-27 23:10:03 -0800

    based on the following: https://dev.to/nilomiranda/building-a-card-memory-game-with-swift-and-swiftui-97a

---
+++ myMatchGame/myMatchGame/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  myMatchGame
+//
+//  Created by Gideon S Crawley on 2/27/21.
+//  Copyright © 2021 Make School. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
